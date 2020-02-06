a44b3cadd754496c1d2da0c0061f5aa08bccf77d
Angelo Aracri <angeloaracri@yahoo.de>
2020-02-06 15:48:04 +0100

    Now using theme

---
+++ scenes/ui/IconLabel.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+enum {ICON_TIME, ICON_LEVEL, ICON_MONEY}
+onready var ICON_PATHS = {
+    ICON_TIME : preload("res://assets/img/ui/icon_time.png"),
+    ICON_LEVEL : preload("res://assets/img/ui/icon_level.png"),
+    ICON_MONEY : preload("res://assets/img/ui/icon_coin.png")
+}
+
+
+export var icon = ICON_MONEY
+export var text = ""
+export var update_on_signal = ""
+
+func _ready():
+    if update_on_signal != "":
+        get_tree().current_scene.connect(update_on_signal, self, "_set_text")
+    update_viz()
+    
+func _set_text(v):
+    text = str(v)
+    update_viz()
+    
+func update_viz():
+    $NinePatchRect/HBoxContainer/TextureRect.texture = ICON_PATHS[icon]
+    $NinePatchRect/HBoxContainer/Label.text = text
+    
--- scenes/ui/Money.gd
+++
@@ -1,7 +0,0 @@
-extends Label
-
-func _ready():
-    get_tree().current_scene.connect("money_changed", self, "money_changed")
-
-func money_changed(money):
-    text = str(money)
