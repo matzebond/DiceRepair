b38a11b7365ef53f074deac90a3f324d6f1b7fbb
Angelo Aracri <angeloaracri@yahoo.de>
2020-02-06 14:25:29 +0100

    Refactored Job

--- scenes/jobs/Job.gd
+++ scenes/jobs/Job.gd
@@ -11,6 +11,10 @@ var money_reward = 5
 var steps_min:int = 2
 var steps_max:int = 3
 var steps:int
+
+var next_job_pos
+var step_scenes = []
+
 var current_step = 0
 
 onready var root = get_tree().current_scene
@@ -19,59 +23,56 @@ onready var root = get_tree().current_scene
 func _ready():
     steps_min = 2 + floor(root.game_round / 2.0)
     steps_max = 2 + ceil(root.game_round / 2.0)
-    update_tool()
+    next_job_pos = $Steps.global_position
+    add_jobs()
+    
 
-func update_tool():
-    
-    # remove all JobSteps
-    for child in $Steps.get_children():
-        $Steps.remove_child(child)
-    
-    # add JobSteps
+func add_jobs():
     steps = rand_range(steps_min, steps_max+1)
     var cur_pos = Vector2(2,0)
+    
+    for i in range(steps):
+        step_scenes.push_back(null)
+        
     for i in range(steps):
         var jobStep = JobStepScene.instance()
         var work = root.rng.randi_range(max(1, 4-root.game_round), 12+root.game_round)
+
+        $Steps.add_child_below_node($Steps/FirstElem, jobStep)
+        $Steps.position.y -= JOB_STEP_SIZE.y + PADDING.y
+        
         jobStep.init(cur_pos, work)
         jobStep.connect("done", self, "_on_JobStep_done")
-        $Steps.add_child(jobStep)
-
-        # calc next pos
-        cur_pos -= JOB_STEP_SIZE
-        cur_pos -= PADDING
-        
-    $Sprite.position = cur_pos
-    position.y -= JOB_STEP_SIZE.y/2
+        jobStep.global_position = next_job_pos
+        step_scenes[steps-1-i] = jobStep
+    
     current_step = 0
-    enable_step(0)
+    enable_step(current_step)
     
     
 func _on_JobStep_done(jobstep):
+
+    $Tween.interpolate_property($Steps, "position:y", $Steps.position.y, $Steps.position.y+JOB_STEP_SIZE.y+PADDING.y, 0.6, Tween.TRANS_EXPO, Tween.EASE_IN, 0.6)
+    $Tween.start()
+    
     current_step += 1
     if current_step >= steps:
         get_tree().current_scene.add_money(money_reward, position)
-    
-    $Tween.interpolate_property(self, "position:y", position.y, position.y+JOB_STEP_SIZE.y+PADDING.y, 0.6, Tween.TRANS_EXPO, Tween.EASE_IN, 0.6)
-    $Tween.start()
-    
-    if $Steps.get_child_count() > 0:
-        $Steps.get_child(0).enable(true)
-    else:
         done()
-    
+    else:
+        enable_step(current_step)
     
 func done():
     emit_signal("completed")
-    $Tween.interpolate_property($Sprite/Inactive, "color:a", $Sprite/Inactive.color.a, 0, 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+    $Tween.interpolate_property($Steps/JobHead/Inactive, "color:a", $Steps/JobHead/Inactive.color.a, 0, 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
     $Tween.start()
         
 func can_finish_step(dice):
     if $Steps.get_child_count() > 0:
-        return $Steps.get_child(0).can_finish(dice)
+        return step_scenes[current_step].can_finish(dice)
     else:
         return true
     
 func enable_step(id):
-    for i in range($Steps.get_child_count()):
-        $Steps.get_child(i).enable((id) == i)
+    for i in range(current_step, len(step_scenes)):
+        step_scenes[i].enable((id) == i)
