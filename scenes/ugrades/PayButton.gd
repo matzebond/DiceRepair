888afac4fe7cbe5a8f023bcfbbf4f26406cce4c4
Angelo Aracri <angeloaracri@yahoo.de>
2020-02-07 19:46:46 +0100

    Increased PayButton font size and changed its layout

--- scenes/ugrades/PayButton.gd
+++ scenes/ugrades/PayButton.gd
@@ -7,9 +7,6 @@ func _ready():
     if not Engine.is_editor_hint():
         get_tree().current_scene.connect("money_changed", self, "money_changed")
     
-func _on_PayButton_draw():
-    $VBoxContainer/CenterContainer/Label.text = text
-    
 func _set_price(v):
     $VBoxContainer/HBoxContainer/Price.text = str(v)
     price = v
