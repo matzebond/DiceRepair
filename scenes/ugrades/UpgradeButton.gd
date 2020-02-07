96f750422a0da47b61997db384f56cac6e038c8f
Angelo Aracri <angeloaracri@yahoo.de>
2020-02-07 15:54:26 +0100

    Added (non-functional upgrade button)

---
+++ scenes/ugrades/UpgradeButton.gd
@@ -0,0 +1,42 @@
+extends "PayButton.gd"
+
+const Die = preload("res://scenes/die/Die.tscn")
+
+class Upgrade:
+    var price = 0
+    var icon_id = 0
+    var text
+    func set_params(price, icon_id, text):
+        self.price = price
+        self.icon_id = icon_id
+        self.text = text
+        
+    func can_use_on(face):
+        return false
+    func use_on(face): # only use when can_use_on() returned true
+        pass
+    func inst_button():
+        var inst = get_script().new()
+        inst.upgrade = self
+        
+        
+class UpgradePlusDelta extends Upgrade:
+    var delta
+    func _init(delta):
+        .set_params(3 * delta, 5, str(delta))
+        pass
+    func can_use_on(face):
+        return face.type == Die.Number
+    func use_on(face):
+        face.value += delta
+
+var upgrade
+func _ready():
+    upgrade = UpgradePlusDelta.new(2) # delete this
+
+    _set_price(upgrade.price)
+    $Icon.frame = upgrade.icon_id
+    $Label.text = upgrade.text
+
+func _on_UpgradeButton_pressed():
+    pass # Replace with function body.
