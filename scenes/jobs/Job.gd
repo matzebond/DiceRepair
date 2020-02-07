808fdd44836191b2bd3e43873a5935281b9c17a0
Angelo Aracri <angeloaracri@yahoo.de>
2020-02-07 12:55:25 +0100

    Initial Job animation, fixed small bugs with mask

--- scenes/jobs/Job.gd
+++ scenes/jobs/Job.gd
@@ -19,16 +19,29 @@ var current_step = 0
 
 onready var root = get_tree().current_scene
 
+export var initial_anim_delay = 0.0
+
 
 func _ready():
     steps_min = 2 + floor(root.game_round / 2.0)
     steps_max = 2 + ceil(root.game_round / 2.0)
+    steps = rand_range(steps_min, steps_max+1)
+    
     next_job_pos = $Steps.global_position
     add_jobs()
     
+    # Start animation
+    var start_y = $Steps.position.y - 600 - JOB_STEP_SIZE.y*steps
+    var end_y = $Steps.position.y
+    $Steps.position.y = start_y
+    $Tween.connect("tween_completed", self, "after_JobStep_anim_done", [], CONNECT_ONESHOT)
+    $Tween.interpolate_property($Steps, "position:y", start_y, end_y, 1.2, Tween.TRANS_EXPO, Tween.EASE_IN, initial_anim_delay)
+    $Tween.start()
+
+func initial_anim_done(_obj, _key):
+    enable_step(current_step)
 
 func add_jobs():
-    steps = rand_range(steps_min, steps_max+1)
     var cur_pos = Vector2(2,0)
     
     for i in range(steps):
@@ -47,7 +60,6 @@ func add_jobs():
         step_scenes[steps-1-i] = jobStep
     
     current_step = 0
-    enable_step(current_step)
     
     
 func _on_JobStep_done(jobstep):
@@ -56,7 +68,7 @@ func _on_JobStep_done(jobstep):
     $Tween.interpolate_property($Steps, "position:y", $Steps.position.y, $Steps.position.y+JOB_STEP_SIZE.y+PADDING.y, 0.6, Tween.TRANS_EXPO, Tween.EASE_IN, 0.6)
     $Tween.start()
 
-func after_JobStep_anim_done(obj, _key):
+func after_JobStep_anim_done(_obj, _key):
     current_step += 1
     if current_step >= steps:
         get_tree().current_scene.add_money(money_reward, $Steps/JobHead.global_position)
